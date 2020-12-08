13fcb318718fd8898bc0b13c7e60800b63b05e62
vitaliyeb <vlvlv.vovov@bk.ru>
2020-12-08 21:15:54 +0300

    сделал рабочий вид

---
+++ scrips.js
@@ -0,0 +1,12 @@
+
+let canvas = document.getElementById('canvas'),
+    ctx = canvas.getContext('2d'),
+    width = window.innerWidth,
+    height = window.innerHeight;
+
+    canvas.width = width;
+    canvas.height = height;
+
+
+
+    
