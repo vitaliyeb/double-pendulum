edb72abec39d8649054a2be479bf1e45ab46ac7d
vitaliyeb <vlvlv.vovov@bk.ru>
2020-12-09 19:56:18 +0300

    рисовалка пути

--- scrips.js
+++ scrips.js
@@ -1,36 +1,45 @@
 
 let canvas = document.getElementById('canvas'),
     ctx = canvas.getContext('2d'),
+    canvas2 = document.getElementById('canvas-line'),
+    ctx2 = canvas2.getContext('2d'),
     width = window.innerWidth,
     height = window.innerHeight;
 
 canvas.width = width;
 canvas.height = height;
 
+canvas2.width = width;
+canvas2.height = height;
+ctx2.translate(width / 2, height / 2);
+ctx.widhtS = 10;
+
 let l1 = 100,
     l2 = 100,
     pi = Math.PI,
     m1 = 10,
     m2 = 10,
-    a1 = pi / 6,
-    a2 = pi / 6,
+    a1 = pi / 2,
+    a2 = pi / 2,
     cos = Math.cos,
     sin = Math.sin,
     a1_v = 0,
     a2_v = 0,
     a1_a = 0, 
     a2_a = 0,
-    g = 1;
+    g = 1,
+    lx = undefined,
+    ly = undefined;
 
 
 
 function draw() {
-    let x1 = l1 * sin(a1),
-        y1 = l1 * cos(a1),
-        a1_a = (-g * (2 * m1 + m2) * sin(a1) - m2 * g * sin(a1 - 2 * a2) - 2 * sin(a1 - a2) * m2 * (a2_v * a2_v * l2 +  a1_v * a1_v * l1 * cos(a1 - a2))) / (l1 * (2 * m1 + m2 - m2 * cos(2 * a1 - 2 * a2))),
+    let a1_a = (-g * (2 * m1 + m2) * sin(a1) - m2 * g * sin(a1 - 2 * a2) - 2 * sin(a1 - a2) * m2 * (a2_v * a2_v * l2 +  a1_v * a1_v * l1 * cos(a1 - a2))) / (l1 * (2 * m1 + m2 - m2 * cos(2 * a1 - 2 * a2))),
         a2_a = (2 * sin(a1 - a2) * (a1_v * a1_v * l1 * (m1 + m2) + g * (m1 + m2) * cos(a1) + a2_v * a2_v * l2 * m2 * cos(a1 - a2))) / (l2 * (2 * m1 + m2 - m2 * cos(2 * a1 - 2 * a2)));
 
 
+    let x1 = l1 * sin(a1),
+        y1 = l1 * cos(a1); 
 
 
     ctx.beginPath();
@@ -57,6 +66,19 @@ function draw() {
     a2_v += a2_a;
     a1 += a1_v;
     a2 += a2_v;
+
+    drawLine(x2, y2);
+}
+
+function drawLine(x, y){
+    if(!lx || !ly){
+        lx = x;
+        ly = y;
+    };
+    ctx2.arcTo(x, y, lx, ly, 5)
+    ctx2.stroke();
+    lx = x;
+    ly = y;   
 }
 
 function reDraw(){
